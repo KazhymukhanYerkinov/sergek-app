9a0df4160196ae37b9c275af47834be763ee04e9
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2022-08-25 01:34:53 +0600

    asddasd

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,7 @@ import { TodosModule } from './todos/todos.module';
 		}),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: process.env.POSTGRES_HOST,
-			port: Number(process.env.POSTGRES_PORT),
-			username: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			database: process.env.POSTGRES_DB,
+			url: process.env.DATABASE_URL,
 			entities: [Todo],
 			synchronize: true,
 		}),
